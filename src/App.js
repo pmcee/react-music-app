fc07fc7206090b7bce4753c441cb303e621e70c0
jiamin.he <jiamin.he@shopee.com>
2022-02-09 17:30:31 +0800

    更改

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import {IconStyle} from './assets/iconfont/iconfont'
+import {Routes, Route, Link} from "react-router-dom";
+import {IconStyle} from './assets/iconfont/iconfont';
 import {GlobalStyle} from './styles'
+import Home from '../application/Home';
+import Recommend from '../application/Recommend';
+import Singers from '../application/Singers';
+import Rank from '../application/Rank';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <i className='iconfont'>&#xe62b;</i>
+      <h1>welcome to react router!</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<Rank />} />
+        <Route path='about' element={<Recommend />} />
+        <Route path='about' element={<Singers />} />
+      </Routes>
     </div>
   )
 }
